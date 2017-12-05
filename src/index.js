22835def2170910413d33b95d830606d84f603a4
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2017-12-05 12:16:36 +0100

    added editorconfig

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const greetins = 'Hello world!';
 
-if (greetins) {
+if  (greetins) {
   const bodyEl = document.body;
   const h1El = document.createElement('h1');
   const textNode = document.createTextNode(greetins);
-  h1El.appendChild(textNode);
+   h1El.appendChild(textNode);
   bodyEl.appendChild(h1El);
 }
